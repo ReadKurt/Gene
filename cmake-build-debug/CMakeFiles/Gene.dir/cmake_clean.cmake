7f3bc90e2b21cf6767841ede3f5fdde1ba72dc44
WJ <hustwj1996@gmail.com>
2017-05-26 23:39:01 +0800

    create Gene

---
+++ cmake-build-debug/CMakeFiles/Gene.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Gene.dir/main.cpp.obj"
+  "Gene.pdb"
+  "Gene.exe"
+  "Gene.exe.manifest"
+  "libGene.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Gene.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
